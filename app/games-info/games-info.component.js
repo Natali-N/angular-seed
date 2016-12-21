8efee001d419f8873788ad0b55c09572e6664c5e
Natali-N <n6091063@gmail.com>
2016-12-21 11:50:11 +0300

    checking todos

--- app/games-info/games-info.component.js
+++ app/games-info/games-info.component.js
@@ -6,14 +6,14 @@ angular.
         templateUrl: 'games-info/games-info.template.html',
         controller: ['GamesService',
             function CurrentGameController(GamesService) {
-
+                var self = this;
                 this.gameStatistics = false;
 
                 function calculatePrecision(arr) {
                     return Math.abs(arr[0]-arr[1]) + Math.abs(arr[1]-arr[2]) + Math.abs(arr[2]-arr[0]);
                 }
 
-                this.updateStatistics = function() {
+                function updateStatistics() {
                     var games = GamesService.getAll();
 
                     function isFinished(game) {
@@ -21,19 +21,19 @@ angular.
                     }
 
                     if (!games.some(isFinished)) {
-                        this.gameStatistics = true;
+                        self.gameStatistics = true;
                         return;
                     }
 
-                    this.players = {};
+                    self.players = {};
 
                     games.forEach(function(game) {
                         if (game.status) {
 
                             game.players.forEach(function(playerName) {
 
-                                if ( !(playerName in this.players)) {
-                                    this.players[playerName] = {
+                                if ( !(playerName in self.players)) {
+                                    self.players[playerName] = {
                                         name: playerName,
                                         bestThrow: 0,
                                         worstThrow: 0,
@@ -43,43 +43,43 @@ angular.
                                         games: []
                                     };
 
-                                    this.players[playerName].bestThrow = game.bestThrow[playerName];
-                                    this.players[playerName].worstThrow = game.worstThrow[playerName];
-                                    this.players[playerName].bestPrecision = game.bestPrecision[playerName];
-                                    this.players[playerName].worstPrecision = game.worstPrecision[playerName];
+                                    self.players[playerName].bestThrow = game.bestThrow[playerName];
+                                    self.players[playerName].worstThrow = game.worstThrow[playerName];
+                                    self.players[playerName].bestPrecision = game.bestPrecision[playerName];
+                                    self.players[playerName].worstPrecision = game.worstPrecision[playerName];
 
                                 } else {
-                                    if (this.players[playerName].bestThrow > game.bestThrow[playerName]) {
-                                        this.players[playerName].bestThrow = game.bestThrow[playerName];
+                                    if (self.players[playerName].bestThrow > game.bestThrow[playerName]) {
+                                        self.players[playerName].bestThrow = game.bestThrow[playerName];
                                     }
 
-                                    if (this.players[playerName].worstThrow < game.worstThrow[playerName]) {
-                                        this.players[playerName].worstThrow = game.worstThrow[playerName];
+                                    if (self.players[playerName].worstThrow < game.worstThrow[playerName]) {
+                                        self.players[playerName].worstThrow = game.worstThrow[playerName];
                                     }
 
-                                    if (calculatePrecision(this.players[playerName].bestPrecision) < calculatePrecision(game.bestPrecision[playerName])) {
-                                        this.players[playerName].bestPrecision = game.bestPrecision[playerName];
+                                    if (calculatePrecision(self.players[playerName].bestPrecision) < calculatePrecision(game.bestPrecision[playerName])) {
+                                        self.players[playerName].bestPrecision = game.bestPrecision[playerName];
                                     }
 
-                                    if (calculatePrecision(this.players[playerName].worstPrecision) > calculatePrecision(game.worstPrecision[playerName])) {
-                                        this.players[playerName].worstPrecision = game.worstPrecision[playerName];
+                                    if (calculatePrecision(self.players[playerName].worstPrecision) > calculatePrecision(game.worstPrecision[playerName])) {
+                                        self.players[playerName].worstPrecision = game.worstPrecision[playerName];
                                     }
                                 }
 
-                                this.players[playerName].games.push(game.title);
+                                self.players[playerName].games.push(game.title);
 
                                 for (var z=0; z<game.winners.length; z++) {
                                     if (game.winners[z] === playerName) {
-                                        this.players[playerName].winner.push(game.title)
+                                        self.players[playerName].winner.push(game.title)
                                     }
 
                                 }
-                            }, this);
+                            }, self);
                         }
-                    }, this);
-                };
+                    }, self);
+                }
 
-                this.updateStatistics();
+                updateStatistics();
             }
         ]
     });
